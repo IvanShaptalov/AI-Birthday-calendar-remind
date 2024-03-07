20c0ea5dcb467a13756a61987358ef2cb001b8d9
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-03-07 21:35:30 +0200

    feat(): added edit, share and copy buttons

--- AI Birthday calendar & remind/Controllers/WishCreator/WishCreateFinishViewController.swift
+++ AI Birthday calendar & remind/Controllers/WishCreator/WishCreateFinishViewController.swift
@@ -6,9 +6,43 @@
 //
 
 import UIKit
+//
+//protocol WishRequestTransferProtocol {
+//    var wishRequest: Wish
+//}
 
 class WishCreateFinishViewController: UIViewController {
+    @IBOutlet weak var gptTextViewField: UITextView!
+    
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var copyButton: UIButton!
+    @IBAction func editButtonPressed(_ sender: Any) {
+        if !self.gptTextViewField.isFocused  {
+            self.gptTextViewField.becomeFirstResponder()
+            self.gptTextViewField.setNeedsFocusUpdate()
+        }
+    }
+    @IBAction func sharePressed(_ sender: Any) {
+        let text = gptTextViewField.text
+                
+                // set up activity view controller
+                let textToShare = [ text! ]
+                let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+                activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+                
+                // exclude some activity types from the list (optional)
+                activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+                
+                // present the view controller
+                self.present(activityViewController, animated: true, completion: nil)
+    }
+    @IBAction func copyPressed(_ sender: Any) {
+        let text = gptTextViewField.text
+        
+        UIPasteboard.general.string = text
 
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
