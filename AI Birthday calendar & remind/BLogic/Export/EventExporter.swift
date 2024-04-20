76d6afc1c1efedde520ad8b549788ed7c7ff1ea4
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-04-20 14:51:40 +0300

    feat(): fix access error

--- AI Birthday calendar & remind/BLogic/Export/EventExporter.swift
+++ AI Birthday calendar & remind/BLogic/Export/EventExporter.swift
@@ -101,98 +101,84 @@ class CalendarEventExporter: EventExporter {
         
         let eventStore = EKEventStore()
         // check access to reminders
-        eventStore.requestAccess(to: EKEntityType.event, completion: {
-            granted, error in
-            if (granted) && (error == nil) {
-                print("granted \(granted)")
-            var cEvents: [EKEvent] = []
-                // event saving to reminder
-                for event in self.events {
-                    let calendarEvent:EKEvent = EKEvent(eventStore: eventStore)
-                    calendarEvent.title = event.title
-                    
-                    // to AppConfiguration.notificationTime
-                    let startDateWithoutDateRule = DateEventFormatter.yearToCurrentInEvent(event)
-                    guard let startDate = DateEventFormatter.notificateTimeToRules(eventDate: startDateWithoutDateRule) else {
-                        continue
-                    }
-                    
-                    calendarEvent.startDate = event.eventType == .simpleEvent ? event.eventDate : startDate
-                    
-                    calendarEvent.endDate = calendarEvent.startDate.addingTimeInterval(60*60)  // 1 hour
-                    
-                    
-                    calendarEvent.calendar = eventStore.defaultCalendarForNewEvents
-                    
-                    cEvents.append(calendarEvent)
-                    
-                }
-                                
-                do {
-                    try cEvents.forEach({try eventStore.save($0, span: EKSpan.futureEvents)})
-                } catch let error as NSError{
-                    NSLog("error while export to calendars: \(error.localizedDescription)")
-                    statusCallback("Cannot export birthdays to calendar", false)
-                    return
-                }
-                statusCallback("Birthdays exported", true)
-            } else {
-                statusCallback("Enable calendar permission", false)
+        
+        var cEvents: [EKEvent] = []
+        // event saving to reminder
+        for event in self.events {
+            let calendarEvent:EKEvent = EKEvent(eventStore: eventStore)
+            calendarEvent.title = event.title
+            
+            // to AppConfiguration.notificationTime
+            let startDateWithoutDateRule = DateEventFormatter.yearToCurrentInEvent(event)
+            guard let startDate = DateEventFormatter.notificateTimeToRules(eventDate: startDateWithoutDateRule) else {
+                continue
             }
-        })
+            
+            calendarEvent.startDate = event.eventType == .simpleEvent ? event.eventDate : startDate
+            
+            calendarEvent.endDate = calendarEvent.startDate.addingTimeInterval(60*60)  // 1 hour
+            
+            
+            calendarEvent.calendar = eventStore.defaultCalendarForNewEvents
+            
+            cEvents.append(calendarEvent)
+            
+        }
+        
+        do {
+            try cEvents.forEach({try eventStore.save($0, span: EKSpan.futureEvents)})
+        } catch let error as NSError{
+            NSLog("error while export to calendars: \(error.localizedDescription)")
+            statusCallback("Cannot export birthdays to calendar", false)
+            return
+        }
+        statusCallback("Birthdays exported", true)
     }
+    
 }
 
 // MARK: - Reminder
 class ReminderEventExporter: EventExporter {
-        
+    
     func export(statusCallback: @escaping(String, Bool) -> Void){
         NSLog("⏰ export reminders")
         
         let eventStore = EKEventStore()
         // check access to reminders
-        eventStore.requestAccess(to: EKEntityType.reminder, completion: {
-            granted, error in
-            if (granted) && (error == nil) {
-                print("granted \(granted)")
-            var reminders: [EKReminder] = []
-                // event saving to reminder
-                for event in self.events {
-                    let reminder:EKReminder = EKReminder(eventStore: eventStore)
-                    reminder.title = event.title
-                    reminder.priority = 2
-                    
-                    // update year to current in birthdays and anniversary
-                    // to AppConfiguration.notificationTime
-                    let alarmTimeWithoutTimeRule = DateEventFormatter.yearToCurrentInEvent(event)
-                    guard let alarmTime = DateEventFormatter.notificateTimeToRules(eventDate: alarmTimeWithoutTimeRule) else {
-                        continue
-                    }
-                    
-                    // if simple event, keep default date
-                    let alarm = EKAlarm(absoluteDate: event.eventType == .simpleEvent ? event.eventDate : alarmTime)
-                    reminder.addAlarm(alarm)
-                    
-                    reminder.calendar = eventStore.defaultCalendarForNewReminders()
-                    
-                    reminders.append(reminder)
-                    
-                }
-                                
-                do {
-                    try reminders.forEach({try eventStore.save($0, commit: true)})
-                } catch {
-                    statusCallback("Cannot export events to reminders", false)
-                    return
-                }
-                statusCallback("Reminders exported", true)
-            } else {
-                statusCallback("Enable reminder permissions", false)
+        
+        var reminders: [EKReminder] = []
+        // event saving to reminder
+        for event in self.events {
+            let reminder:EKReminder = EKReminder(eventStore: eventStore)
+            reminder.title = event.title
+            reminder.priority = 2
+            
+            // update year to current in birthdays and anniversary
+            // to AppConfiguration.notificationTime
+            let alarmTimeWithoutTimeRule = DateEventFormatter.yearToCurrentInEvent(event)
+            guard let alarmTime = DateEventFormatter.notificateTimeToRules(eventDate: alarmTimeWithoutTimeRule) else {
+                continue
             }
-        })
+            
+            // if simple event, keep default date
+            let alarm = EKAlarm(absoluteDate: event.eventType == .simpleEvent ? event.eventDate : alarmTime)
+            reminder.addAlarm(alarm)
+            
+            reminder.calendar = eventStore.defaultCalendarForNewReminders()
+            
+            reminders.append(reminder)
+            
+        }
         
-        
+        do {
+            try reminders.forEach({try eventStore.save($0, commit: true)})
+        } catch {
+            statusCallback("Cannot export events to reminders", false)
+            return
+        }
+        statusCallback("Reminders exported", true)
     }
+    
 }
 
 
